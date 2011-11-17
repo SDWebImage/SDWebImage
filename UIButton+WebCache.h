ae3aab90994d7ca82a2a1ee0fbedf0d5f040627a
Fernando Saragoca <fsaragoca@me.com>
2011-11-17 15:21:57 -0200

    Documented categories (appledoc)

--- UIButton+WebCache.h
+++ UIButton+WebCache.h
@@ -11,8 +11,31 @@
 
 @interface UIButton (WebCache) <SDWebImageManagerDelegate>
 
+/**
+ Set the imageView `image` with an `url`.
+ 
+ The downloand is asynchronous and cached.
+ 
+ @param url The url that the image is found.
+ @see setImageWithURL:placeholderImage:
+ */
 - (void)setImageWithURL:(NSURL *)url;
+
+/**
+ Set the imageView `image` with an `url` and a placeholder.
+ 
+ The downloand is asynchronous and cached.
+ 
+ @param url The url that the `image` is found.
+ @param placeholder A `image` that will be visible while loading the final image.
+ */
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder;
+
+/**
+ Cancel the current download
+ 
+ This method should be called when downloading a lot of images (`tableView`) and/or large images.
+ */
 - (void)cancelCurrentImageLoad;
 
 @end
