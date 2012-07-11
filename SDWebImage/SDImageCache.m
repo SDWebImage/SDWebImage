146c8fc12ce907a615c9f4a381b5540cdaf3703d
Kevin Monahan <kevin@nivektric.com>
2012-07-11 15:30:02 -0400

    Add the ability to set the max cache age.

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -123,6 +123,10 @@ static natural_t get_free_memory(void)
     return instance;
 }
 
++ (void) setMaxCacheAge:(NSInteger)maxCacheAge {
+    cacheMaxCacheAge = maxCacheAge;
+}
+
 #pragma mark SDImageCache (private)
 
 - (NSString *)cachePathForKey:(NSString *)key
