6b53b9b32be63743baa19dea968baa787682e3ca
Florent Vilmart <vfloz@me.com>
2012-09-01 13:23:44 -0400

    Added support for optimized images that will automatically download the @2x version If in retina environment

--- SDWebImage/UIImageView+WebCache.m
+++ SDWebImage/UIImageView+WebCache.m
@@ -8,15 +8,56 @@
 
 #import "UIImageView+WebCache.h"
 
+@interface UIImageView (WebCachePrivate)
+
+-(void) retinaURL:(NSURL *) url;
+-(BOOL) isRetina;
+@end
+
 @implementation UIImageView (WebCache)
+
+
+-(BOOL) isRetina{
+    return ([UIScreen mainScreen].scale == 2.0f);
+}
+
+-(void) retinaURL:(NSURL *) url{
+    NSString * lastComponent = [url lastPathComponent];
+    NSLog(@"Last Component %@", lastComponent);
+    
+    NSString *extension = [url pathExtension];
+    if ([lastComponent rangeOfString:@"@2x"].location == NSNotFound) {
+         //NSString* newURLString = [[url absoluteString] substringToIndex:fragmentRange.location + fragmentRange.length];
+        NSString * replacedLastComponent = [lastComponent stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@".%@",extension] withString:[NSString stringWithFormat:@"@2x.%@",extension]];
+        NSLog(@"Replaced %@",replacedLastComponent);
+        url = [NSURL URLWithString:[[url absoluteString] stringByReplacingOccurrencesOfString:lastComponent withString:replacedLastComponent]];
+    }
+}
 
 - (void)setImageWithURL:(NSURL *)url
 {
+    
+    [self setImageWithURL:url placeholderImage:nil];
+}
+
+- (void)setOptimizedImageWithURL:(NSURL *)url
+{
+    if ([self isRetina]) {
+        [self retinaURL:url];
+    }
     [self setImageWithURL:url placeholderImage:nil];
 }
 
 - (void)setImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder
 {
+    [self setImageWithURL:url placeholderImage:placeholder options:0];
+}
+
+- (void)setOptimizedImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder
+{
+    if ([self isRetina]) {
+        [self retinaURL:url];
+    }
     [self setImageWithURL:url placeholderImage:placeholder options:0];
 }
 
@@ -31,6 +72,23 @@
 
     if (url)
     {
+        [manager downloadWithURL:url delegate:self options:options];
+    }
+}
+- (void)setOptimizedImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options
+{
+    SDWebImageManager *manager = [SDWebImageManager sharedManager];
+    
+    // Remove in progress downloader from queue
+    [manager cancelForDelegate:self];
+    
+    self.image = placeholder;
+
+    if (url)
+    {
+        if ([self isRetina]) {
+            [self retinaURL:url];
+        }
         [manager downloadWithURL:url delegate:self options:options];
     }
 }
@@ -57,6 +115,33 @@
 
     if (url)
     {
+        [manager downloadWithURL:url delegate:self options:options success:success failure:failure];
+    }
+}
+- (void)setOptimizedImageWithURL:(NSURL *)url success:(void (^)(UIImage *image))success failure:(void (^)(NSError *error))failure;
+{
+    [self setOptimizedImageWithURL:url placeholderImage:nil success:success failure:failure];
+}
+
+- (void)setOptimizedImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder success:(void (^)(UIImage *image))success failure:(void (^)(NSError *error))failure;
+{
+    [self setOptimizedImageWithURL:url placeholderImage:placeholder options:0 success:success failure:failure];
+}
+
+- (void)setOptimizedImageWithURL:(NSURL *)url placeholderImage:(UIImage *)placeholder options:(SDWebImageOptions)options success:(void (^)(UIImage *image))success failure:(void (^)(NSError *error))failure;
+{
+    SDWebImageManager *manager = [SDWebImageManager sharedManager];
+    
+    // Remove in progress downloader from queue
+    [manager cancelForDelegate:self];
+    
+    self.image = placeholder;
+    
+    if (url)
+    {
+        if ([self isRetina]) {
+            [self retinaURL:url];
+        }
         [manager downloadWithURL:url delegate:self options:options success:success failure:failure];
     }
 }
