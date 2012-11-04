efe65bfd3beb65c9333f363e98858f926474fcd7
Olivier Poitrey <rs@dailymotion.com>
2012-11-04 17:49:10 +0100

    Fix source file headers

--- SDWebImage/SDWebImageDownloaderOperation.h
+++ SDWebImage/SDWebImageDownloaderOperation.h
@@ -1,10 +1,10 @@
-//
-//  SDWebImageDownloaderOperation.h
-//  SDWebImage
-//
-//  Created by Olivier Poitrey on 04/11/12.
-//  Copyright (c) 2012 Dailymotion. All rights reserved.
-//
+/*
+ * This file is part of the SDWebImage package.
+ * (c) Olivier Poitrey <rs@dailymotion.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 #import <Foundation/Foundation.h>
 #import "SDWebImageDownloader.h"
