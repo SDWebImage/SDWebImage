d3b5fd2bd2211bb54a237d58d9156d887e50d6ea
Olivier Poitrey <rs@dailymotion.com>
2012-12-11 16:11:06 +0100

    Fix ARC compilation warning breaking some non-ARC projects (fix #229)

--- SDWebImage/SDWebImageCompat.h
+++ SDWebImage/SDWebImageCompat.h
@@ -13,10 +13,6 @@
 #error SDWebImage does not support Objective-C Garbage Collection
 #endif
 
-#if !__has_feature(objc_arc)
-#error SDWebImage is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
-#endif
-
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_5_0
 #error SDWebImage doesn't support Deployement Target version < 5.0
 #endif
@@ -57,43 +53,4 @@
     #define SDDispatchQueueSetterSementics assign
 #endif
 
-NS_INLINE UIImage *SDScaledImageForPath(NSString *path, NSObject *imageOrData)
-{
-    if (!imageOrData)
-    {
-        return nil;
-    }
-
-    UIImage *image = nil;
-    if ([imageOrData isKindOfClass:[NSData class]])
-    {
-        image = [[UIImage alloc] initWithData:(NSData *)imageOrData];
-    }
-    else if ([imageOrData isKindOfClass:[UIImage class]])
-    {
-        image = (UIImage *)imageOrData;
-    }
-    else
-    {
-        return nil;
-    }
-
-    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)])
-    {
-        CGFloat scale = 1.0;
-        if (path.length >= 8)
-        {
-            // Search @2x. at the end of the string, before a 3 to 4 extension length (only if key len is 8 or more @2x. + 4 len ext)
-            NSRange range = [path rangeOfString:@"@2x." options:0 range:NSMakeRange(path.length - 8, 5)];
-            if (range.location != NSNotFound)
-            {
-                scale = 2.0;
-            }
-        }
-
-        UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale:scale orientation:image.imageOrientation];
-        image = scaledImage;
-    }
-
-    return image;
-}
+extern UIImage *SDScaledImageForPath(NSString *path, NSObject *imageOrData);
