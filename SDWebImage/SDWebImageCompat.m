55ebe66b4c3b4f2fad0d7f597832b620cfbfad18
Bastian Müller <bm@issuu.com>
2013-04-23 09:26:35 +0200

    Fix and make SDScaledImageForPath extensible

--- SDWebImage/SDWebImageCompat.m
+++ SDWebImage/SDWebImageCompat.m
@@ -12,34 +12,15 @@
 #error SDWebImage is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
 #endif
 
-inline UIImage *SDScaledImageForPath(NSString *path, NSObject *imageOrData)
+inline UIImage *SDScaledImageForKey(NSString *key, UIImage *image)
 {
-    if (!imageOrData)
-    {
-        return nil;
-    }
-
-    UIImage *image = nil;
-    if ([imageOrData isKindOfClass:[NSData class]])
-    {
-        image = [[UIImage alloc] initWithData:(NSData *)imageOrData];
-    }
-    else if ([imageOrData isKindOfClass:[UIImage class]])
-    {
-        image = (UIImage *)imageOrData;
-    }
-    else
-    {
-        return nil;
-    }
-
     if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)])
     {
         CGFloat scale = 1.0;
-        if (path.length >= 8)
+        if (key.length >= 8)
         {
             // Search @2x. at the end of the string, before a 3 to 4 extension length (only if key len is 8 or more @2x. + 4 len ext)
-            NSRange range = [path rangeOfString:@"@2x." options:0 range:NSMakeRange(path.length - 8, 5)];
+            NSRange range = [key rangeOfString:@"@2x." options:0 range:NSMakeRange(key.length - 8, 5)];
             if (range.location != NSNotFound)
             {
                 scale = 2.0;
@@ -49,6 +30,5 @@ inline UIImage *SDScaledImageForPath(NSString *path, NSObject *imageOrData)
         UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale:scale orientation:image.imageOrientation];
         image = scaledImage;
     }
-
     return image;
 }
