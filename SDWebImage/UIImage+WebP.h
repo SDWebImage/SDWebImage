63805ef590763092c9f820c9fc9cd1737bceedb1
Alex Soto <alex@alexsoto.me>
2013-06-11 18:57:07 -0500

    // Fix for issue #416 Undefined symbols for architecture armv7 since WebP introduction when deploying to device
    Added Functions to resolve some undefined symbols when using WebP and force_load flag
    
    void WebPInitPremultiplyNEON(void);
    void WebPInitUpsamplersNEON(void);
    void VP8DspInitNEON(void);
    
    Changes under MIT License

--- SDWebImage/UIImage+WebP.h
+++ SDWebImage/UIImage+WebP.h
@@ -9,6 +9,11 @@
 #ifdef SD_WEBP
 #import <UIKit/UIKit.h>
 
+// Fix for issue #416 Undefined symbols for architecture armv7 since WebP introduction when deploying to device
+void WebPInitPremultiplyNEON(void);
+void WebPInitUpsamplersNEON(void);
+void VP8DspInitNEON(void);
+
 @interface UIImage (WebP)
 
 + (UIImage *)sd_imageWithWebPData:(NSData *)data;
