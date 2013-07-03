4c903eb8df18c7fdbed6210defa45c00db92ec13
limboy <limboy.me@gmail.com>
2013-07-03 17:11:46 +0800

    Add WebP Detect Category

---
+++ SDWebImage/NSData+WebP.h
@@ -0,0 +1,15 @@
+//
+//  NSData+WebP.h
+//  SDWebImage
+//
+//  Created by Limboy on 7/3/13.
+//  Copyright (c) 2013 Dailymotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (WebP)
+
+- (BOOL)sd_isWebP;
+
+@end
