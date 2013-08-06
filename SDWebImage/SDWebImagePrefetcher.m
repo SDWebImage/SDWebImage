5f6a40fb2885d23b064399c3ce200d088873bc39
Philip Plotnikov <Philip@plotnikov.nl>
2013-08-06 15:54:40 +0200

    Removed loggin
    Signed-off-by: Philip Plotnikov <Philip@plotnikov.nl>

--- SDWebImage/SDWebImagePrefetcher.m
+++ SDWebImage/SDWebImagePrefetcher.m
@@ -63,11 +63,11 @@
 
         if (image)
         {
-            NSLog(@"Prefetched %d out of %d", self.finishedCount, self.prefetchURLs.count);
+//            NSLog(@"Prefetched %d out of %d", self.finishedCount, self.prefetchURLs.count);
         }
         else
         {
-            NSLog(@"Prefetched %d out of %d (Failed)", self.finishedCount, [self.prefetchURLs count]);
+//            NSLog(@"Prefetched %d out of %d (Failed)", self.finishedCount, [self.prefetchURLs count]);
 
             // Add last failed
             self.skippedCount++;
