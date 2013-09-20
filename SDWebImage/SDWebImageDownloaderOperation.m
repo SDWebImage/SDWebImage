03d28fee03dbcb0e0d0f0d0851158b77a10ea6cb
Olivier Poitrey <rs@dailymotion.com>
2013-09-20 10:46:35 -0700

    Only use the runloop with timeout on iOS 5 (fix #497)

--- SDWebImage/SDWebImageDownloaderOperation.m
+++ SDWebImage/SDWebImageDownloaderOperation.m
@@ -70,10 +70,18 @@
         }
         [[NSNotificationCenter defaultCenter] postNotificationName:SDWebImageDownloadStartNotification object:self];
 
-        // Make sure to run the runloop in our background thread so it can process downloaded data
-        // Note: we use a timeout to work around an issue with NSURLConnection cancel under iOS 5
-        //       not waking up the runloop, leading to dead threads (see https://github.com/rs/SDWebImage/issues/466)
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 10, false);
+        if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_5_1)
+        {
+            // Make sure to run the runloop in our background thread so it can process downloaded data
+            // Note: we use a timeout to work around an issue with NSURLConnection cancel under iOS 5
+            //       not waking up the runloop, leading to dead threads (see https://github.com/rs/SDWebImage/issues/466)
+            CFRunLoopRunInMode(kCFRunLoopDefaultMode, 10, false);
+        }
+        else
+        {
+            CFRunLoopRun();
+        }
+
         if (!self.isFinished)
         {
             [self.connection cancel];
