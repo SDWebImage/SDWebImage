d020aea02fb808ee13e3dccbd910046f48bafe7b
Matthew Knippen <matthew@zwiffer.com>
2013-11-26 16:54:59 -0600

    added currentDownload Count, to allow someone to see how many operations are left.

--- SDWebImage/SDWebImageDownloader.m
+++ SDWebImage/SDWebImageDownloader.m
@@ -104,6 +104,10 @@ static NSString *const kCompletedCallbackKey = @"completed";
     _downloadQueue.maxConcurrentOperationCount = maxConcurrentDownloads;
 }
 
+- (NSUInteger)currentDownloadCount {
+    return _downloadQueue.operationCount;
+}
+
 - (NSInteger)maxConcurrentDownloads
 {
     return _downloadQueue.maxConcurrentOperationCount;
