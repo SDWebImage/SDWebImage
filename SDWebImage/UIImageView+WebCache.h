77be243598990976a4b78f5e9549bd9c29bf61f2
Klaas Pieter Annema <klaaspieter@annema.me>
2013-11-29 11:39:01 -0500

    Add documentation to imageURL property

--- SDWebImage/UIImageView+WebCache.h
+++ SDWebImage/UIImageView+WebCache.h
@@ -44,6 +44,12 @@
  */
 @interface UIImageView (WebCache)
 
+/**
+ * Get the current image URL.
+ *
+ * Note that because of the limitations of categories this property can get out of sync
+ * if you use setImage: directly.
+ */
 - (NSURL *)imageURL;
 
 /**
