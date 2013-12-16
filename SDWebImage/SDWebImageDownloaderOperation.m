f8d26f517246885f884bcca32fea579cc7161128
Erik Charlebois <erikcharlebois@gmail.com>
2013-12-16 14:41:35 -0500

    Track current thread in downloader operation

--- SDWebImage/SDWebImageDownloaderOperation.m
+++ SDWebImage/SDWebImageDownloaderOperation.m
@@ -85,6 +85,7 @@
 
         self.executing = YES;
         self.connection = [NSURLConnection.alloc initWithRequest:self.request delegate:self startImmediately:NO];
+        self.thread = [NSThread currentThread];
     }
 
     [self.connection start];
