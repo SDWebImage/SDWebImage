3284e157ffe961eabc747f5b891e20ad22568b04
Bogdan Poplauschi <bpoplauschi@gmail.com>
2014-06-12 14:58:32 +0300

    Documented SDWebImageDownloaderOperation initWithRequest:options:progress:completed:cancelled: method. Fixes #526

--- SDWebImage/SDWebImageDownloaderOperation.h
+++ SDWebImage/SDWebImageDownloaderOperation.h
@@ -15,6 +15,21 @@
 @property (strong, nonatomic, readonly) NSURLRequest *request;
 @property (assign, nonatomic, readonly) SDWebImageDownloaderOptions options;
 
+/**
+ *  Initializes a `SDWebImageDownloaderOperation` object
+ *
+ *  @see SDWebImageDownloaderOperation
+ *
+ *  @param request        the URL request
+ *  @param options        downloader options
+ *  @param progressBlock  the block executed when a new chunk of data arrives. 
+ *                        @note the progress block is executed on a background queue
+ *  @param completedBlock the block executed when the download is done. 
+ *                        @note the completed block is executed on the main queue for success. If errors are found, there is a chance the block will be executed on a background queue
+ *  @param cancelBlock    the block executed if the download (operation) is cancelled
+ *
+ *  @return the initialized instance
+ */
 - (id)initWithRequest:(NSURLRequest *)request
               options:(SDWebImageDownloaderOptions)options
              progress:(SDWebImageDownloaderProgressBlock)progressBlock
