35f0347555399efbf81132d85610837b84385d8d
TAKeanice <TAKeanice@users.noreply.github.com>
2014-09-26 20:35:57 +0200

    Update UIImage+GIF.m

--- SDWebImage/UIImage+GIF.m
+++ SDWebImage/UIImage+GIF.m
@@ -95,21 +95,33 @@
     NSData *data;
     NSString *path;
     
-    //get gif for 3x display
+    //get gif for @3x display
     if (scale > 2.0f) {
         path = [[NSBundle mainBundle] pathForResource:threeTimesResName ofType:@"gif"];
         data = [NSData dataWithContentsOfFile:path];
     }
     
-    //get gif for 2x display or if there is no 3x resource
+    //get gif for 2x display or if there is no @3x resource
     if (scale > 1.0f && !data) {
         path = [[NSBundle mainBundle] pathForResource:twoTimesResName ofType:@"gif"];
         data = [NSData dataWithContentsOfFile:path];
     }
     
-    //get gif for non-retina display or if there is no 2x or 3x resource
+    //get gif for non-retina display or if there is no @2x or 3x resource
     if (!data) {
         path = [[NSBundle mainBundle] pathForResource:normalResName ofType:@"gif"];
+        data = [NSData dataWithContentsOfFile:path];
+    }
+    
+    //no data yet? maybe there is only a @2x image
+    if (!data) {
+        path = [[NSBundle mainBundle] pathForResource:twoTimesResName ofType:@"gif"];
+        data = [NSData dataWithContentsOfFile:path];
+    }
+    
+    //still no data? could be only a @3x image is provided
+    if (!data) {
+        path = [[NSBundle mainBundle] pathForResource:threeTimesResName ofType:@"gif"];
         data = [NSData dataWithContentsOfFile:path];
     }
     
@@ -120,43 +132,6 @@
     //still no resource found --> try to return non-gif image
     return [UIImage imageNamed:name];
     
-}
-
-- (UIImage *)sd_animatedImageByScalingAndCroppingToSize:(CGSize)size {
-    if (CGSizeEqualToSize(self.size, size) || CGSizeEqualToSize(size, CGSizeZero)) {
-        return self;
-    }
-
-    CGSize scaledSize = size;
-    CGPoint thumbnailPoint = CGPointZero;
-
-    CGFloat widthFactor = size.width / self.size.width;
-    CGFloat heightFactor = size.height / self.size.height;
-    CGFloat scaleFactor = (widthFactor > heightFactor) ? widthFactor : heightFactor;
-    scaledSize.width = self.size.width * scaleFactor;
-    scaledSize.height = self.size.height * scaleFactor;
-
-    if (widthFactor > heightFactor) {
-        thumbnailPoint.y = (size.height - scaledSize.height) * 0.5;
-    }
-    else if (widthFactor < heightFactor) {
-        thumbnailPoint.x = (size.width - scaledSize.width) * 0.5;
-    }
-
-    NSMutableArray *scaledImages = [NSMutableArray array];
-
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
-
-    for (UIImage *image in self.images) {
-        [image drawInRect:CGRectMake(thumbnailPoint.x, thumbnailPoint.y, scaledSize.width, scaledSize.height)];
-        UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-
-        [scaledImages addObject:newImage];
-    }
-
-    UIGraphicsEndImageContext();
-
-    return [UIImage animatedImageWithImages:scaledImages duration:self.duration];
 }
 
 @end
