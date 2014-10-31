62a8ee1ab6ba3cd6a7fb87fc04f87ebd012ebd82
Bogdan Poplauschi <bpoplauschi@gmail.com>
2014-10-31 19:12:42 +0200

    Fixed #774 - remove deprecated methods. Since 4.0 is a major release, we don't need the backwards compatible methods

--- SDWebImage/NSData+ImageContentType.h
+++ SDWebImage/NSData+ImageContentType.h
@@ -17,10 +17,3 @@
 + (NSString *)sd_contentTypeForImageData:(NSData *)data;
 
 @end
-
-
-@interface NSData (ImageContentTypeDeprecated)
-
-+ (NSString *)contentTypeForImageData:(NSData *)data __deprecated_msg("Use `sd_contentTypeForImageData:`");
-
-@end
