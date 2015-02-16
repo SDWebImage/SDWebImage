6468a92f3250b671a3228fe3c6dac85f3ea35d5e
Evgeny Petrov <john07.ru@gmail.com>
2015-02-16 15:56:30 +0300

    allow override diskCachePath

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -74,8 +74,7 @@ BOOL ImageDataHasPNGPreffix(NSData *data) {
         _memCache.name = fullNamespace;
 
         // Init the disk cache
-        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-        _diskCachePath = [paths[0] stringByAppendingPathComponent:fullNamespace];
+        _diskCachePath = [self makeDiskCachePath:fullNamespace];
 
         dispatch_sync(_ioQueue, ^{
             _fileManager = [NSFileManager new];
@@ -143,6 +142,12 @@ BOOL ImageDataHasPNGPreffix(NSData *data) {
 }
 
 #pragma mark ImageCache
+
+// Init the disk cache
+-(NSString *)makeDiskCachePath:(NSString*)fullNamespace{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    return [paths[0] stringByAppendingPathComponent:fullNamespace];
+}
 
 - (void)storeImage:(UIImage *)image recalculateFromImage:(BOOL)recalculate imageData:(NSData *)imageData forKey:(NSString *)key toDisk:(BOOL)toDisk {
     if (!image || !key) {
