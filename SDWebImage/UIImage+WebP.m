8829c4f9d613c46374564182982679002fb30a6e
lixf <leestarxin@gmail.com>
2015-11-25 17:48:51 +0800

    WebP support animation
    reference from
    http://stackoverflow.com/questions/28231580/error-decoding-animated-webp
    -ios

--- SDWebImage/UIImage+WebP.m
+++ SDWebImage/UIImage+WebP.m
@@ -8,12 +8,9 @@
 
 #ifdef SD_WEBP
 #import "UIImage+WebP.h"
-
-#if !COCOAPODS
 #import "webp/decode.h"
-#else
-#import "libwebp/webp/decode.h"
-#endif
+#import <webp/demux.h>
+#import <webp/mux_types.h>
 
 // Callback for CGDataProviderRelease
 static void FreeImageData(void *info, const void *data, size_t size)
@@ -24,6 +21,82 @@ static void FreeImageData(void *info, const void *data, size_t size)
 @implementation UIImage (WebP)
 
 + (UIImage *)sd_imageWithWebPData:(NSData *)data {
+    
+    UIImage *image = [UIImage sd_animatedWebPImageWithWebPData:data];
+    if (image) {
+        return image;
+    } else {
+        return [UIImage sd_webpImageWithWithWebPData:data];
+    }
+    
+}
+
++ (UIImage *)sd_animatedWebPImageWithWebPData:(NSData *)data {
+    UIImage *result = nil;
+    WebPData webpData;
+    WebPDataInit(&webpData);
+    webpData.bytes = (const uint8_t *)[data bytes];
+    webpData.size = [data length];
+    WebPDemuxer* demux = WebPDemux(&webpData);
+    uint32_t flags = WebPDemuxGetI(demux, WEBP_FF_FORMAT_FLAGS);
+    
+    if (flags & ANIMATION_FLAG) {
+        WebPIterator iter;
+        if (WebPDemuxGetFrame(demux, 1, &iter)) {
+            
+            WebPDecoderConfig config;
+            WebPInitDecoderConfig(&config);
+            
+            int width = WebPDemuxGetI(demux, WEBP_FF_CANVAS_WIDTH);
+            int height = WebPDemuxGetI(demux, WEBP_FF_CANVAS_HEIGHT);
+            
+            config.input.height = height;
+            config.input.width = width;
+            config.input.has_alpha = iter.has_alpha;
+            config.input.has_animation = 1;
+            config.options.no_fancy_upsampling = 1;
+            config.options.bypass_filtering = 1;
+            config.options.use_threads = 1;
+            config.output.colorspace = MODE_RGBA;
+            
+            CGFloat frameDuration = iter.duration;
+            NSMutableArray *images = [NSMutableArray array];
+            do {
+                WebPData frame = iter.fragment;
+                
+                VP8StatusCode status = WebPDecode(frame.bytes, frame.size, &config);
+                if (status != VP8_STATUS_OK) {
+                    NSLog(@"Error decoding frame");
+                }
+                
+                uint8_t *data = WebPDecodeRGBA(frame.bytes, frame.size, &width, &height);
+                CGDataProviderRef provider = CGDataProviderCreateWithData(&config, data, config.options.scaled_width  * config.options.scaled_height * 4, NULL);
+                
+                CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+                CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaLast;
+                CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
+                
+                CGImageRef imageRef = CGImageCreate(width, height, 8, 32, 4 * width, colorSpaceRef, bitmapInfo, provider, NULL, YES, renderingIntent);
+                UIImage *image = [UIImage imageWithCGImage:imageRef];
+                if (image) {
+                    [images addObject:image];
+                }
+                CGImageRelease(imageRef);
+                CGColorSpaceRelease(colorSpaceRef);
+                CGDataProviderRelease(provider);
+                
+            } while (WebPDemuxNextFrame(&iter));
+            WebPDemuxReleaseIterator(&iter);
+            NSTimeInterval duration = [images count] * frameDuration / 1000;
+            result = [UIImage animatedImageWithImages:images duration:duration];
+        }
+    }
+    WebPDemuxDelete(demux);
+    
+    return result;
+}
+
++ (UIImage *)sd_webpImageWithWithWebPData:(NSData *)data {
     WebPDecoderConfig config;
     if (!WebPInitDecoderConfig(&config)) {
         return nil;
