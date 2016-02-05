8826031a9145cc94f66e433ea0c4e7681054e972
Mikael <mikael@pangea.com>
2016-02-05 15:32:18 +0900

    Updated dispatch_ macro in order to avoid redefinition when included as Pod

--- SDWebImage/SDWebImageCompat.h
+++ SDWebImage/SDWebImageCompat.h
@@ -57,16 +57,20 @@ typedef void(^SDWebImageNoParamsBlock)();
 
 extern NSString *const SDWebImageErrorDomain;
 
+#ifndef dispatch_main_sync_safe
 #define dispatch_main_sync_safe(block)\
     if ([NSThread isMainThread]) {\
         block();\
     } else {\
         dispatch_sync(dispatch_get_main_queue(), block);\
     }
+#endif
 
+#ifndef dispatch_main_async_safe
 #define dispatch_main_async_safe(block)\
     if ([NSThread isMainThread]) {\
         block();\
     } else {\
         dispatch_async(dispatch_get_main_queue(), block);\
     }
+#endif
