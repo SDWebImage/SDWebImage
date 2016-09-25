fbca6a6dc3bdb3da1bfba21c0cd7a0ff5f66bfd2
Bogdan Poplauschi <bpoplauschi@gmail.com>
2016-09-25 14:57:51 +0300

    Removed unneded code

--- Tests/Tests/UIImageMultiFormatTests.m
+++ Tests/Tests/UIImageMultiFormatTests.m
@@ -22,18 +22,6 @@
 
 @implementation UIImageMultiFormatTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testImageOrientationFromImageDataWithInvalidData {
     // sync download image
     SEL selector = @selector(sd_imageOrientationFromImageData:);
