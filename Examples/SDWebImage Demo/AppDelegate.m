6831a09a95fda4a099c2949c424390b2dd95927c
ptlCoder <pengtanglong_local@163.com>
2018-04-17 14:34:04 +0800

    --

--- Examples/SDWebImage Demo/AppDelegate.m
+++ Examples/SDWebImage Demo/AppDelegate.m
@@ -33,5 +33,17 @@
     return YES;
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
 
 @end
