991fd899854340ed18a8fc871cebd23b5372f776
zhongwuzw <zhongwuzw@qq.com>
2018-09-12 23:04:45 +0800

    Fix memory cache image cost miscalculation for 4.x

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -18,7 +18,7 @@ FOUNDATION_STATIC_INLINE NSUInteger SDCacheCostForImage(UIImage *image) {
 #if SD_MAC
     return image.size.height * image.size.width;
 #elif SD_UIKIT || SD_WATCH
-    return image.size.height * image.size.width * image.scale * image.scale;
+    return image.size.height * image.size.width * image.scale * image.scale * CGImageGetBitsPerPixel(image.CGImage) / 8;
 #endif
 }
 
