f0a4014aa95f0bbf538ed9eae6333d7a887e1484
zhongwuzw <zhongwuzw@qq.com>
2019-01-31 12:01:04 +0800

    Update the comment to show why we need filter GIF

--- SDWebImage/SDWebImageDownloaderOperation.m
+++ SDWebImage/SDWebImageDownloaderOperation.m
@@ -419,7 +419,8 @@ didReceiveResponse:(NSURLResponse *)response
                             NSString *key = [[SDWebImageManager sharedManager] cacheKeyForURL:self.request.URL];
                             image = [self scaledImageForKey:key image:image];
                             
-                            // Do not force decoding animated images
+                            // Do not force decoding animated images or GIF,
+                            // because there has imageCoder which can change `image` or `imageData` to static image, lose the animated feature totally.
                             BOOL shouldDecode = !image.images && image.sd_imageFormat != SDImageFormatGIF;
                             if (shouldDecode) {
                                 if (self.shouldDecompressImages) {
