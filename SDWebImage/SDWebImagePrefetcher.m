402d06b93ee4a08ec43072635005d84ca65c37f1
zhongwuzw <zhongwuzw@qq.com>
2019-03-16 00:16:01 +0800

    Remove shadow variable warning

--- SDWebImage/SDWebImagePrefetcher.m
+++ SDWebImage/SDWebImagePrefetcher.m
@@ -107,11 +107,11 @@
     for (NSURL *url in token.urls) {
         __weak typeof(self) wself = self;
         SDAsyncBlockOperation *prefetchOperation = [SDAsyncBlockOperation blockOperationWithBlock:^(SDAsyncBlockOperation * _Nonnull asyncOperation) {
-             __strong typeof(wself) sself = wself;
-             if (!sself) {
+             __strong typeof(wself) strongSelf = wself;
+             if (!strongSelf) {
                  return;
              }
-             id<SDWebImageOperation> operation = [sself.manager loadImageWithURL:url options:sself.options context:sself.context progress:nil completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
+             id<SDWebImageOperation> operation = [strongSelf.manager loadImageWithURL:url options:strongSelf.options context:strongSelf.context progress:nil completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
                  __strong typeof(wself) sself = wself;
                  if (!sself) {
                      return;
