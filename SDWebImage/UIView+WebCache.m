428899a041dcb5b529212011aef8aa8c734af784
DreamPiggy <lizhuoli1126@126.com>
2019-06-05 16:56:58 +0800

    fix compatability for Xcode 11

--- SDWebImage/UIView+WebCache.m
+++ SDWebImage/UIView+WebCache.m
@@ -335,7 +335,7 @@ static char TAG_ACTIVITY_SHOW;
 }
 
 - (void)sd_setIndicatorStyle:(UIActivityIndicatorViewStyle)style{
-    objc_setAssociatedObject(self, &TAG_ACTIVITY_STYLE, [NSNumber numberWithInt:style], OBJC_ASSOCIATION_RETAIN);
+    objc_setAssociatedObject(self, &TAG_ACTIVITY_STYLE, [NSNumber numberWithInteger:style], OBJC_ASSOCIATION_RETAIN);
 }
 
 - (int)sd_getIndicatorStyle{
