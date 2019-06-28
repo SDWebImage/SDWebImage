665405000537c52ea1c88bcfd59d50187395f82d
DreamPiggy <lizhuoli1126@126.com>
2019-06-28 16:29:59 +0800

    Fix typo

--- SDWebImage/SDAnimatedImage.h
+++ SDWebImage/SDAnimatedImage.h
@@ -86,7 +86,7 @@
 
 /**
  Current animated image data, you can use this to grab the compressed format data and create another animated image instance.
- If this image instance is a animated image created by using a animated image coder (which means using the API listed above or using `initWithAnimatedCoder:scale:`), this property is non-nil.
+ If this image instance is an animated image created by using animated image coder (which means using the API listed above or using `initWithAnimatedCoder:scale:`), this property is non-nil.
  */
 @property (nonatomic, copy, readonly, nullable) NSData *animatedImageData;
 
