9746ba164ab013c9ddef298cda6773ad3983c35d
DreamPiggy <lizhuoli1126@126.com>
2019-07-12 17:02:23 +0800

    Update the documentation for stateful API

--- SDWebImage/UIView+WebCacheState.h
+++ SDWebImage/UIView+WebCacheState.h
@@ -12,14 +12,47 @@
 typedef NSString * SDWebImageStateContainerKey NS_EXTENSIBLE_STRING_ENUM;
 typedef NSDictionary<SDWebImageStateContainerKey, id> SDWebImageStateContainer;
 typedef NSMutableDictionary<SDWebImageStateContainerKey, id> SDWebImageMutableStateContainer;
+/**
+ Key for current loading URL (NSURL *)
+ */
 FOUNDATION_EXPORT SDWebImageStateContainerKey _Nonnull const SDWebImageStateContainerURL;
+/**
+ Key for current loading progress (NSProgress *)
+ */
 FOUNDATION_EXPORT SDWebImageStateContainerKey _Nonnull const SDWebImageStateContainerProgress;
+/**
+ Key for current image transition animation (SDWebImageTransition *)
+ */
 FOUNDATION_EXPORT SDWebImageStateContainerKey _Nonnull const SDWebImageStateContainerTransition;
 
+/**
+ These methods are used for WebCache view which have multiple states for image loading, for example, `UIButton` or `UIImageView.highlightedImage`
+ It maitain the state container for per-operation, make it possible for control and check each image loading operation's state.
+ @note For developer who want to add SDWebImage View Category support for their own stateful class, learn more on Wiki.
+ */
 @interface UIView (WebCacheState)
 
+/**
+ Get the image loading state container for specify operation key
+
+ @param key key for identifying the operations
+ @return The image loading state container
+ */
 - (nullable SDWebImageStateContainer *)sd_imageLoadStateForKey:(nullable NSString *)key;
+
+/**
+ Set the image loading state container for specify operation key
+
+ @param state The image loading state container
+ @param key key for identifying the operations
+ */
 - (void)sd_setImageLoadState:(nullable SDWebImageStateContainer *)state forKey:(nullable NSString *)key;
+
+/**
+ Rmove the image loading state container for specify operation key
+
+ @param key key for identifying the operations
+ */
 - (void)sd_removeImageLoadStateForKey:(nullable NSString *)key;
 
 @end
