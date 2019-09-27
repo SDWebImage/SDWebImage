d91ea19768c179cd8a996fef1e2f71c76df7d697
DreamPiggy <lizhuoli1126@126.com>
2019-09-27 11:37:48 +0800

    Remove the HEIC coder from default coder list. Since it contains performance issue and may block main thread. Needs to check in the future iOS release

--- SDWebImage/Core/SDImageHEICCoder.h
+++ SDWebImage/Core/SDImageHEICCoder.h
@@ -14,6 +14,7 @@
  Image/IO provide the static HEIC (.heic) support in iOS 11/macOS 10.13/tvOS 11/watchOS 4+.
  Image/IO provide the animated HEIC (.heics) support in iOS 13/macOS 10.15/tvOS 13/watchOS 6+.
  See https://nokiatech.github.io/heif/technical.html for the standard.
+ @note This coder is not in the default coder list for now, since HEIC animated image is really rare, and Apple's implementation still contains performance issues. You can enable if you need this.
  @note If you need to support lower firmware version for HEIF, you can have a try at https://github.com/SDWebImage/SDWebImageHEIFCoder
  */
 @interface SDImageHEICCoder : SDImageIOAnimatedCoder <SDProgressiveImageCoder, SDAnimatedImageCoder>
