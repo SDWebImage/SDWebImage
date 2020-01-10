d29dfda82a90981b630e1e30f862d21f6596998f
DreamPiggy <lizhuoli1126@126.com>
2020-01-10 19:30:17 +0800

    Update the Example to use thumbnail decoding

--- Examples/SDWebImage Demo/MasterViewController.m
+++ Examples/SDWebImage Demo/MasterViewController.m
@@ -115,7 +115,8 @@
     cell.customTextLabel.text = [NSString stringWithFormat:@"Image #%ld", (long)indexPath.row];
     [cell.customImageView sd_setImageWithURL:[NSURL URLWithString:self.objects[indexPath.row]]
                             placeholderImage:placeholderImage
-                                     options:indexPath.row == 0 ? SDWebImageRefreshCached : 0];
+                                     options:indexPath.row == 0 ? SDWebImageRefreshCached : 0
+                                     context:@{SDWebImageContextImageThumbnailPixelSize : @(CGSizeMake(100, 100))}];
     return cell;
 }
 
