1ce44a12b0558dc1df5111fc1835a1be1c9d6759
DreamPiggy <lizhuoli1126@126.com>
2020-04-04 14:19:08 +0800

    Change the behavior to return the abstract for unknown UTI type, this can solve the accident issue for custom coder who provide a new format

--- SDWebImage/Core/NSData+ImageContentType.h
+++ SDWebImage/Core/NSData+ImageContentType.h
@@ -45,6 +45,7 @@ static const SDImageFormat SDImageFormatSVG       = 8;
  *
  *  @param format Format as SDImageFormat
  *  @return The UTType as CFStringRef
+ *  @note For unknown format, `kUTTypeImage` abstract type will return
  */
 + (nonnull CFStringRef)sd_UTTypeFromImageFormat:(SDImageFormat)format CF_RETURNS_NOT_RETAINED NS_SWIFT_NAME(sd_UTType(from:));
 
@@ -53,6 +54,7 @@ static const SDImageFormat SDImageFormatSVG       = 8;
  *
  *  @param uttype The UTType as CFStringRef
  *  @return The Format as SDImageFormat
+ *  @note For unknown type, `SDImageFormatUndefined` will return
  */
 + (SDImageFormat)sd_imageFormatFromUTType:(nonnull CFStringRef)uttype;
 
