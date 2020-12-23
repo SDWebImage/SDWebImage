c88472442d8a0cd677cfea7502a4a3c2669f1713
Insomnia <282132315@qq.com>
2020-12-23 20:15:16 +0800

    mod: change filename hash to base64 safe url

--- SDWebImage/Core/SDDiskCache.m
+++ SDWebImage/Core/SDDiskCache.m
@@ -9,7 +9,6 @@
 #import "SDDiskCache.h"
 #import "SDImageCacheConfig.h"
 #import "SDFileAttributeHelper.h"
-#import <CommonCrypto/CommonDigest.h>
 
 static NSString * const SDDiskCacheExtendedAttributeName = @"com.hackemist.SDDiskCache";
 
@@ -299,28 +298,21 @@ static NSString * const SDDiskCacheExtendedAttributeName = @"com.hackemist.SDDis
 
 #pragma mark - Hash
 
-#define SD_MAX_FILE_EXTENSION_LENGTH (NAME_MAX - CC_SHA256_DIGEST_LENGTH * 2 - 1)
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 static inline NSString * _Nonnull SDDiskCacheFileNameForKey(NSString * _Nullable key) {
-    const char *str = key.UTF8String;
-    if (str == NULL) {
-        str = "";
+    
+    key = [key stringByReplacingOccurrencesOfString:@"+" withString:@"-"];
+    key = [key stringByReplacingOccurrencesOfString:@"/" withString:@"-"];
+    if (key.length % 4 != 0) {
+        int cnt = 4 - key.length % 4;
+        while (cnt--) {
+            key = [key stringByAppendingString:@"="];
+        }
     }
-    unsigned char r[CC_SHA256_DIGEST_LENGTH];
-    CC_SHA1(str, (CC_LONG)strlen(str), r);
-    NSURL *keyURL = [NSURL URLWithString:key];
-    NSString *ext = keyURL ? keyURL.pathExtension : key.pathExtension;
-    // File system has file name length limit, we need to check if ext is too long, we don't add it to the filename
-    if (ext.length > SD_MAX_FILE_EXTENSION_LENGTH) {
-        ext = nil;
-    }
-    NSString *filename = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%@",
-                          r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10],
-                          r[11], r[12], r[13], r[14], r[15], r[16], r[17], r[18], r[19], r[20], r[21], r[22], r[23], r[24], r[25], r[26], r[27], r[28], r[29], ext.length == 0 ? @"" : [NSString stringWithFormat:@".%@", ext]];
+    
+    NSData *data = [key dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *filename = [data base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    
     return filename;
 }
-#pragma clang diagnostic pop
 
 @end
