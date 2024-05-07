48bb2c9604fa09c573a351340a48d50e2558efa7
DreamPiggy <lizhuoli1126@126.com>
2024-05-07 20:43:42 +0800

    Update the Github CI for release

--- Scripts/sign-xcframework.sh
+++ Scripts/sign-xcframework.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 set -o pipefail
 
 if [ -z "$SRCROOT" ]
@@ -9,7 +10,7 @@ then
 fi
 
 # Self-sign XCFramework
-if [ -z CODESIGN_KEY_BASE64 ]; then
+if [ -z $CODESIGN_KEY_BASE64 ]; then
     echo "Ignore Codesign XCFramework! You must sign SDWebImage before shipping to App Store. See: https://developer.apple.com/support/third-party-SDK-requirements"
     exit 0
 fi
@@ -24,10 +25,10 @@ echo $CODESIGN_KEY_BASE64 | base64 -D > "$(PWD)/Certificate/${CODESIGN_IDENTIFY_
 security create-keychain -p "$KEYCHAIN_PASSWORD" ios.keychain
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN
 
-security import "$(PWD)/Certificate/${CODESIGN_IDENTIFY_NAME}.cer" -k $KEYCHAIN -T /usr/bin/codesign -T /usr/bin/security
-security import "$(PWD)/Certificate/${CODESIGN_IDENTIFY_NAME}.p12" -k $KEYCHAIN -P "$KEY_PASSWORD" -T /usr/bin/codesign -T /usr/bin/security
+security import "$(PWD)/Certificate/${CODESIGN_IDENTIFY_NAME}.cer" -k $KEYCHAIN -T /usr/bin/codesign
+security import "$(PWD)/Certificate/${CODESIGN_IDENTIFY_NAME}.p12" -k $KEYCHAIN -P "$KEY_PASSWORD" -T /usr/bin/codesign
 security list-keychains -s ios.keychain
-security set-key-partition-list -S "apple-tool:,apple:" -k "$KEYCHAIN_PASSWORD" $KEYCHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k "$KEYCHAIN_PASSWORD" $KEYCHAIN
 
 echo "Codesign XCFramework"
 /usr/bin/codesign --force --timestamp -v --sign "SDWebImage Signing Certificate" "${SRCROOT}/build/SDWebImage.xcframework"
