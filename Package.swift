d947006a57e8a86ac019781f8cfb27e719aae8e1
Yuva <94527987+yuvashrikarunakaran@users.noreply.github.com>
2025-01-04 14:30:42 +0530

    Update Package.swift
    Here’s the updated Package.swift file with the following changes

--- Package.swift
+++ Package.swift
@@ -9,30 +9,36 @@ let package = Package(
         .macOS(.v10_11),
         .iOS(.v9),
         .tvOS(.v9),
-        .watchOS(.v2)
+        .watchOS(.v2),
+        .visionOS(.v1) // Added visionOS support
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SDWebImage",
-            targets: ["SDWebImage"]),
+            targets: ["SDWebImage"]
+        ),
         .library(
             name: "SDWebImageMapKit",
-            targets: ["SDWebImageMapKit"])
+            targets: ["SDWebImageMapKit"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // Example: .package(url: "https://github.com/Example/Example.git", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets are the basic building blocks of a package.
+        // A target can define a module or a test suite.
+        // Targets can depend on other targets in this package and products in packages which this package depends on.
         .target(
             name: "SDWebImage",
             dependencies: [],
             path: "SDWebImage",
             sources: ["Core", "Private"],
-            resources: [.copy("Resources/PrivacyInfo.xcprivacy")],
+            resources: [
+                .copy("Resources/PrivacyInfo.xcprivacy")
+            ],
             cSettings: [
                 .headerSearchPath("Core"),
                 .headerSearchPath("Private")
@@ -43,7 +49,9 @@ let package = Package(
             dependencies: ["SDWebImage"],
             path: "SDWebImageMapKit",
             sources: ["MapKit"],
-            resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
+            resources: [
+                .copy("Resources/PrivacyInfo.xcprivacy")
+            ]
         )
     ]
 )
