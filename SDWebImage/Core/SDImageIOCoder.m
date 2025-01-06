22ecfc840862626ce426f2a17158199407f598bd
Jia Song <593908937@qq.com>
2025-01-06 10:02:42 +0800

    HDR, encoding properties add kCGImageDestinationEncodeToISOGainmap, compatible with SDR displays while preserving HDR

--- SDWebImage/Core/SDImageIOCoder.m
+++ SDWebImage/Core/SDImageIOCoder.m
@@ -377,6 +377,11 @@ static NSString * kSDCGImageDestinationRequestedFileSize = @"kCGImageDestination
     }
     
     NSMutableDictionary *properties = [NSMutableDictionary dictionary];
+    if (@available(macOS 15, iOS 18, tvOS 18.0, watchOS 18.0, *)) {
+        if (image.isHighDynamicRange) {
+            properties[(__bridge NSString *)kCGImageDestinationEncodeRequest] = (__bridge NSString *)kCGImageDestinationEncodeToISOGainmap;
+        }
+    }
 #if SD_UIKIT || SD_WATCH
     CGImagePropertyOrientation exifOrientation = [SDImageCoderHelper exifOrientationFromImageOrientation:image.imageOrientation];
 #else
